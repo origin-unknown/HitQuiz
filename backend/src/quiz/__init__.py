2d784f92ec079b428712e5dfb1e754dbe7d7a656
origin-unknown <ich.hier.wer.dort@gmx.de>
2023-05-16 13:38:05 +0200

    Code cleaned

--- backend/src/quiz/__init__.py
+++ backend/src/quiz/__init__.py
@@ -12,7 +12,6 @@ migrate = Migrate()
 def create_app():
 	app = Flask(__name__, 
 	    static_url_path = '',
-	    # static_folder='../client/public/'
 	)
 	app.config.from_object(Config)
 	db.init_app(app)
